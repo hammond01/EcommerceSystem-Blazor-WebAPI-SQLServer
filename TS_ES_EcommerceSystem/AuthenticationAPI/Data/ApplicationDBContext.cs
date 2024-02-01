e3bea115ccbb5cae06bdda0b8dc32ec24ec4f353
Hammond <hieutruonghoang01@gmail.com>
2024-02-01 17:00:48 +0700

    Add document ApplicationDBContext

--- TS_ES_EcommerceSystem/AuthenticationAPI/Data/ApplicationDBContext.cs
+++ TS_ES_EcommerceSystem/AuthenticationAPI/Data/ApplicationDBContext.cs
@@ -6,9 +6,12 @@ namespace AuthenticationAPI.Data
 {
     public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationDBContext"/> class.
+        /// </summary>
+        /// <param name="opt">The options for this context.</param>
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> opt) : base(opt)
         {
-
         }
     }
 }
