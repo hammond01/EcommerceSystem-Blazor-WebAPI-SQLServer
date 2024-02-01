f445bfb532fb085f63437991ace4a0b8c7342c6e
Hammond <hieutruonghoang01@gmail.com>
2024-02-01 16:59:27 +0700

    Add document AccountController

--- TS_ES_EcommerceSystem/AuthenticationAPI/Controllers/AccountsController.cs
+++ TS_ES_EcommerceSystem/AuthenticationAPI/Controllers/AccountsController.cs
@@ -1,23 +1,42 @@
-﻿using AuthenticationAPI.Models;
+﻿
+using AuthenticationAPI.Models;
 using AuthenticationAPI.Repositories.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationAPI.Controllers
 {
+    /// <summary>
+    /// Provides endpoints for managing user accounts.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class AccountsController : ControllerBase
     {
         private readonly IAccountServices repo;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountsController"/> class.
+        /// </summary>
+        /// <param name="_repo">The repository.</param>
         public AccountsController(IAccountServices _repo)
         {
             repo = _repo;
         }
+
+        /// <summary>
+        /// Registers a new user account.
+        /// </summary>
+        /// <param name="register">The user account information.</param>
+        /// <returns>A <see cref="IActionResult"/> indicating whether the operation succeeded.</returns>
         [HttpPost("Register")]
         public async Task<IActionResult> Register(Register register)
         {
+            /// <summary>
+            /// Registers a new user account.
+            /// </summary>
+            /// <param name="register">The user account information.</param>
+            /// <returns>A <see cref="IActionResult"/> indicating whether the operation succeeded.</returns>
             var result = await repo.Register(register);
             if (result.Succeeded)
             {
@@ -25,25 +44,54 @@ namespace AuthenticationAPI.Controllers
             }
             return BadRequest();
         }
+
+        /// <summary>
+        /// Logs in an existing user account.
+        /// </summary>
+        /// <param name="login">The user account information.</param>
+        /// <returns>The user's authentication token.</returns>
         [HttpPost("Login")]
         public async Task<IActionResult> Login(Login login)
         {
+            /// <summary>
+            /// Logs in an existing user account.
+            /// </summary>
+            /// <param name="login">The user account information.</param>
+            /// <returns>The user's authentication token.</returns>
             var result = await repo.LoginAsync(login);
             return Ok(result);
         }
 
+        /// <summary>
+        /// Creates a new employee account.
+        /// </summary>
+        /// <param name="registerEmployee">The employee account information.</param>
+        /// <returns>A <see cref="IActionResult"/> indicating whether the operation succeeded.</returns>
         [HttpPost("create-employee")]
         [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> CreateEmployee(RegisterEmployee registerEmployee)
         {
+            /// <summary>
+            /// Creates a new employee account.
+            /// </summary>
+            /// <param name="registerEmployee">The employee account information.</param>
+            /// <returns>A <see cref="IActionResult"/> indicating whether the operation succeeded.</returns>
             var result = await repo.RegisterEmployee(registerEmployee);
             return Ok(result);
         }
 
+        /// <summary>
+        /// Gets a list of available user roles.
+        /// </summary>
+        /// <returns>A list of available user roles.</returns>
         [HttpGet("GetRoles")]
         [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> GetRoles()
         {
+            /// <summary>
+            /// Gets a list of available user roles.
+            /// </summary>
+            /// <returns>A list of available user roles.</returns>
             var result = await repo.GetRoles();
             return Ok(result);
         }
